ead2fb7a342c5b29e859b0a6132c89485873e3f3
veasnapovsr <veasnavarapov@gmail.com>
2024-12-23 18:32:16 +0700

    Add files via upload

---
+++ my-github-actions-project/test/test.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+
+describe('Simple test', function() {
+    it('should return hello message', function() {
+        assert.equal('Hello, GitHub Actions!', 'Hello, GitHub Actions!');
+    });
+});
